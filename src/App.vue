706f4a91ac8ecbfafa2935993120db3d36acdb91
Ирина Черняева <iridache@192.168.1.7>
2023-12-06 18:45:55 +0300

    vuetify added searchpanel draft done

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
-<div>
-
-</div>
+  <h1>Book Search</h1>
+  <HomePage></HomePage>
 </template>
 
 <script>
+import HomePage from "./components/HomePage";
+
+export default {
+  components: {
+    HomePage,
+  },
+};
 
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,29 @@
+<template>
+  <form @submit.prevent="search">
+    <div>
+      <v-text-field variant="outlined" placeholder="Search..." />
+      <v-btn variant="outlined">Search</v-btn>
+    </div>
+    <v-select
+      :items="[
+        'all',
+        'art',
+        'biography',
+        'computers',
+        'history',
+        'medical',
+        'poetry',
+      ]"
+      variant="outlined"
+    ></v-select>
+    <label>Order by</label>
+    <v-radio-group inline>
+      <v-radio label="Relevance" value="relevance"></v-radio>
+      <v-radio label="Newest" value="newest"></v-radio>
+    </v-radio-group>
+  </form>
+</template>
+
+<script></script>
+
+<style scoped></style>
